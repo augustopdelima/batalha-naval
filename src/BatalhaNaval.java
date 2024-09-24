7ffcc2ba6606290b26f6b4c6c3f5d593d1045360
Augusto Pasquali de Lima <augusto.pasqualidelima@gmail.com>
2024-09-24 20:33:49 -0300

    Criando métodos para posicionar navios automaticamente

--- src/BatalhaNaval.java
+++ src/BatalhaNaval.java
@@ -1,9 +1,59 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class BatalhaNaval {
     static int TAMANHO_TABULEIRO = 10;
     static char SIMBOLO_AGUA = '~';
     static String COLUNAS = "ABCDEFGHIJK";
+    static int[] NAVIOS_ORDEM = {4,3,3,2,2,2,1,1,1,1};
+    static char HORIZONTAL = 'H';
+    static char VERTICAL = 'V';
+
+    public static boolean tentarPosicionarNavio(char [][] tabuleiro, int linha, int coluna, int tamanhoNavio, char orientacao) {
+
+
+        if(orientacao == HORIZONTAL) {
+            if(coluna + tamanhoNavio > TAMANHO_TABULEIRO) return false;
+
+            for(int i = 0; i < tamanhoNavio; i++) {
+                char celulaTabuleiro = tabuleiro[linha][coluna + i];
+                boolean temNavio = Character.isDigit(celulaTabuleiro);
+                if(temNavio) return false;
+            }
+
+            for(int i=0; i< tamanhoNavio; i++) {
+                tabuleiro[linha][coluna+i] = Character.forDigit(tamanhoNavio,10);
+            }
+
+        } else {
+            if(linha + tamanhoNavio > TAMANHO_TABULEIRO) return false;
+
+            for(int i=0; i < tamanhoNavio; i++) {
+                char celulaTabuleiro = tabuleiro[linha + i][coluna];
+                boolean temNavio = Character.isDigit(celulaTabuleiro);
+                if(temNavio) return false;
+            }
+
+            for(int i=0; i < tamanhoNavio; i++) {
+                tabuleiro[linha+i][coluna] = Character.forDigit(tamanhoNavio,10);
+            }
+        }
+
+        return true;
+    }
+
+    public static void posicionarNaviosAutomaticamente(char [][] tabuleiro) {
+        Random random = new Random();
+        for(int navio: NAVIOS_ORDEM) {
+            boolean posicionado = false;
+            while (!posicionado) {
+                int linha = random.nextInt(TAMANHO_TABULEIRO);
+                int coluna = random.nextInt(TAMANHO_TABULEIRO);
+                char orientacao = random.nextBoolean() ? HORIZONTAL : VERTICAL;
+                posicionado = tentarPosicionarNavio(tabuleiro, linha, coluna, navio,orientacao);
+            }
+        }
+    }
 
     public static char[][] criaTabuleiro() {
         char[][] tabuleiro = new char[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
@@ -18,14 +68,14 @@ public class BatalhaNaval {
     }
 
     public static void exibirTabuleiro(char[][] tabuleiro, boolean ocultarNavios) {
-        System.out.printf("%4s","");//espaço para alinhar letras nas colunas
+        System.out.printf("%3s","");//espaço para alinhar letras nas colunas
         for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
             System.out.printf("%3s",COLUNAS.charAt(i)+" ");
         }
         System.out.println();
 
         for (int linha = 0; linha < TAMANHO_TABULEIRO ; linha++) {
-            System.out.printf("%3d",linha + 1);
+            System.out.printf("%2d",linha + 1);
             for(int coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++) {
                 char celulaTabuleiro = tabuleiro[linha][coluna];
 
@@ -73,7 +123,13 @@ public class BatalhaNaval {
       char[][] tabuleiro2 = criaTabuleiro();
       Scanner scanner = new Scanner(System.in);
 
-      exibirMenu(scanner);
+      posicionarNaviosAutomaticamente(tabuleiro1);
+
+      exibirTabuleiro(tabuleiro1,false);
+
+
+
+
 
    }
 }
