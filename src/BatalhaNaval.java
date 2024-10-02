c90bc3287f7914079b54c464f8d660a427e7b9d6
DKrupp03 <dikruppengelmann@gmail.com>
2024-10-02 09:24:50 -0300

    feat: ajustando quantidade de navios

--- src/BatalhaNaval.java
+++ src/BatalhaNaval.java
@@ -9,8 +9,7 @@ public class BatalhaNaval {
     static char SIMBOLO_NAVIO = 'X';
     static char SIMBOLO_OCULTO = '?';
     static String COLUNAS = "ABCDEFGHIJ";
-    //static int[] NAVIOS_ORDEM = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
-    static int[] NAVIOS_ORDEM = {4, 3, 2};
+    static int[] NAVIOS_ORDEM = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1};
     static char HORIZONTAL = 'H';
     static char VERTICAL = 'V';
     static int totalNaviosJogador1 = 20;
